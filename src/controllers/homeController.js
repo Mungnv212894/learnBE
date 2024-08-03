dc573815e0a61a23c894cbee79e7663bcc1da7f4
Mung <vanmungxuanduong61@gmail.com>
2024-08-04 00:59:18 +0700

    update len Home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -17,23 +17,47 @@ const viduEJS = (req, res) => {
     res.render('sample'); // Đảm bảo bạn có file 'sample.ejs' trong thư mục views
 };
 
-const postCreateUser=(req,res)=>{
+const postCreateUser= async(req,res)=>{
     let email = req.body.email;
     let name = req.body.name;
     let city = req.body.city;
 
     //res.send('Create a new user')
-    connection.query(
+    // connection.query(
+    //     `INSERT INTO User (email,name,city) 
+    //     VALUE(?,?,?);`,
+    //     [email,name,city],
+    //     function(err,results){
+    //         console.log(results);
+    //         res.send('Create a new user successfully');
+    //     }
+    // );
+    let[results,fields]=await connection.query(
         `INSERT INTO User (email,name,city) 
         VALUE(?,?,?);`,
-        [email,name,city],
-        function(err,results){
-            console.log(results);
-            res.send('Create a new user');
-        }
+        [email,name,city]
     );
+
+    console.log(">>>Results=",results);
+    res.send('Create a new user successfully');
+
+//   connection.query(
+//   'SELECT * FROM  User u ',
+//   function(err, results, fields) {
+//     console.log(">>>Result=",results); // results contains rows returned by server
+//     //console.log(">>>Fields=",fields); // fields contains extra meta data about results, if available
+//   }
+// );
+
+
+
+  //const [results,fields]=await connection.query('SELECT * FROM  User u ');
+ //console.log(">>>Results=",results);
 }
 
+const getCreatePage=(req,res)=>{
+     res.render('create.ejs')
+}
 module.exports = {
-    getHomePage, getABC, viduEJS,postCreateUser
+    getHomePage, getABC, viduEJS,postCreateUser,getCreatePage
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getHomePage, getABC, viduEJS,postCreateUser } = require('../controllers/homeController');
+const { getHomePage, getABC, viduEJS,postCreateUser ,getCreatePage} = require('../controllers/homeController');
 
 router.get('/', getHomePage);
 router.get('/abc', getABC);
 router.get('/test', viduEJS);
 
+router.get('/create',getCreatePage);
 router.post('/create-user',postCreateUser)
 module.exports = router;
